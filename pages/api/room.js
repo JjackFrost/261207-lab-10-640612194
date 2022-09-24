09236c7b73c5a0ffb7722cd59ce97036516f66cd
JjackFrost <nongjj_jay@hotmail.com>
2022-09-24 16:28:46 +0700

    finished

--- pages/api/room.js
+++ pages/api/room.js
@@ -2,4 +2,15 @@ import { readDB } from "../../backendLibs/dbLib";
 
 export default function roomRoute(req, res) {
   const rooms = readDB();
+  const results = [];
+  for (const room of rooms) {
+    results.push({
+      roomId: room.roomId,
+      roomName: room.roomName,
+    });
+  }
+  return res.json({
+    ok: true,
+    results,
+  });
 }
